316df2417f4dfef84ff7e77ff0eb5ef4446c1cc7
SteffenBerlin <Steffen.Baumgarten@gmx.de>
2016-01-18 20:55:46 -0800

    Background picture files and renaming of dancer files

---
+++ src/britishDancer.js
@@ -0,0 +1,23 @@
+var makeBritishDancer = function(top, left, timeBetweenSteps) {
+  makeDancer.call(this, top, left, timeBetweenSteps);
+  this.$node = $('<span class="britishdancer"></span>');
+
+  this.step(timeBetweenSteps);
+  this.setPosition(this.top, this.left);
+  // console.log(top, "top from rose dancer")
+  // console.log(left, "left from rose dancer")
+  // console.log(this.left, "left")
+
+
+};
+  
+makeBritishDancer.prototype = Object.create(makeDancer.prototype);
+
+makeBritishDancer.prototype.constructor = makeBritishDancer;
+
+makeBritishDancer.prototype.step = function() {
+
+  makeDancer.prototype.step.call(this);
+
+  this.$node.toggle();
+};
---
+++ src/germanDancer.js
@@ -0,0 +1,23 @@
+var makeGermanDancer = function(top, left, timeBetweenSteps) {
+  makeDancer.call(this, top, left, timeBetweenSteps);
+  this.$node = $('<span class="germandancer"></span>');
+
+  this.step(timeBetweenSteps);
+  this.setPosition(this.top, this.left);
+  // console.log(top, "top from blue dancer")
+  // console.log(left, "left from blue dancer")
+  // console.log(this.left, "left")
+
+
+};
+  
+makeGermanDancer.prototype = Object.create(makeDancer.prototype);
+
+makeGermanDancer.prototype.constructor = makeGermanDancer;
+
+makeGermanDancer.prototype.step = function() {
+
+  makeDancer.prototype.step.call(this);
+
+  this.$node.toggle();
+};
